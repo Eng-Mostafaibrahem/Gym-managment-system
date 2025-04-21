b1c140cfb0bd9957427b2b4a54f7f85e49c40709
Eng-Mostafaibrahem <eng.mostafaibrahem99@gmail.com>
2025-04-21 03:45:39 +0200

    enhance code

--- DB/db-connection.js
+++ DB/db-connection.js
@@ -12,19 +12,17 @@ export const sequelize = new Sequelize(
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
     dialect: process.env.DB_DIALECT,
-    dialectModule: mysql2, // تحديد mysql2 كموديل للاتصال
-    logging: false, // شيلها لو عايز تشوف الاستعلامات
+    dialectModule: mysql2,
+    logging: false,
   }
 );
 
-// دالة التأكد من الاتصال
 export const connectDB = async () => {
   try {
-    await sequelize.authenticate(); // يتأكد من الاتصال
+    await sequelize.authenticate();
     console.log("✅ Database connected successfully.");
 
-    // لو عايز تزامن الجداول مع قاعدة البيانات
-    await sequelize.sync({ alter: true }); // تقدر تستخدم force: true لو عايز تمسح الجداول وتعيد إنشائها
+    await sequelize.sync({ alter: true });
     console.log("✅ Tables synced successfully.");
   } catch (error) {
     console.error("❌ Unable to connect to the database:", error.message);
